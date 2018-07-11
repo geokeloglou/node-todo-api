491e4778730c7b4c53520a1814a7936d84cab35f
geokeloglou <geokeloglou@gmail.com>
2018-07-11 05:28:08 +0300

    initial commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,33 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+    if(err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+    const db = client.db('TodoApp');
+
+    // db.collection('Todos').find({
+    //     _id: new ObjectID('5b445927ca00cd0d5228dcdb')
+    // }).toArray().then((docs) => {
+    //     console.log('Todos');
+    //     console.log(JSON.stringify(docs, undefined, 4));
+    // }, (err) => {
+    //     console.log('Unable to fetch todos', err);
+    // });
+
+    // db.collection('Todos').find().count().then((count) => {
+    //     console.log(`Todos count: ${count}`);
+    // }, (err) => {
+    //     console.log('Unable to fetch todos', err);
+    // });
+    // client.close();
+
+    db.collection('Users').find({name: 'George'}).toArray().then((docs) => {
+            console.log('Users named George ');
+            console.log(JSON.stringify(docs, undefined, 4));
+        }, (err) => {
+            console.log('Unable to fetch Georges', err);
+        });
+});
